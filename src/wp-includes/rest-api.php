f626cbe9edd9f370d4a34ad1137d8bf65b385898
Adam Silverstein <adam@10up.com>
2017-04-26 17:41:05 -0400

    _fields keys: trim off outside whitespace from the comma delimited list.

--- src/wp-includes/rest-api.php
+++ src/wp-includes/rest-api.php
@@ -619,13 +619,14 @@ function rest_send_allow_header( $response, $server, $request ) {
 }
 
 /**
- * Filter the API response to include only a whitelisted set of response object fields.
+ * Filter the API response to include only a white-listed set of response object fields.
  *
- * @since 4.7.0
+ * @since 4.8.0
  *
  * @param WP_REST_Response $response Current response being served.
  * @param WP_REST_Server   $server   ResponseHandler instance (usually WP_REST_Server).
  * @param WP_REST_Request  $request  The request that was used to make current response.
+ *
  * @return WP_REST_Response Response to be served, trimmed down to contain a subset of fields.
  */
 function rest_filter_response_fields( $response, $server, $request ) {
@@ -637,12 +638,12 @@ function rest_filter_response_fields( $response, $server, $request ) {
 
 	$fields = (array) explode( ',', $request['_fields'] );
 
+	// Trim off outside whitespace from the comma delimited list.
+	$fields = array_map( 'trim', $fields );
+
 	$fields_as_keyed = array_combine( $fields, array_fill( 0, count( $fields ), true ) );
 
 	if ( wp_is_numeric_array( $data ) ) {
-		// $new_data = array_map( function ( $item ) use ( $fields_as_keyed ) {
-		// 	return array_intersect_key( $item, $fields_as_keyed );
-		// }, $data );
 		$new_data = array();
 		foreach ( $data as $item ) {
 			$new_data[] = array_intersect_key( $item, $fields_as_keyed );
